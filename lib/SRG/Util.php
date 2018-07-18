99791ae087b7a771e100eb19ff7de559edb72936
Moritz Schepp <moritz.schepp@gmail.com>
2018-07-18 12:48:40 +1000

    fixes alternating timezone conversion

--- lib/SRG/Util.php
+++ lib/SRG/Util.php
@@ -17,12 +17,10 @@
         $date = new \DateTime($date);
       }
       
-      # We pull this from the db where we only write utc timestamps (but without
+      # We pull dates from the db where we only write utc timestamps (but without
       # the timezone information). Therefore we don't convert them to utc (a
-      # second time)
-      // $date->setTimezone(new \DateTimeZone("UTC"));
-      # RFC 7231
-      return $date->format('D, d M Y H:i:s e');
+      # second time) but we assume utc.
+      return $date->format('D, d M Y H:i:s \G\M\T'); # RFC 7231
     }
 
     public static function to_db_date($date) {
